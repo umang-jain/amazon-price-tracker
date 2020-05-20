99e7da67c3408bb98106a6241ce3a42914a1b3c7
umang-jain <umang.usict.042164@ipu.ac.in>
2020-05-20 23:40:32 +0530

    add nodemailer

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const puppeteer = require('puppeteer');
 const $ = require('cheerio');
 const CronJob = require('cron').CronJob;
 const nodemailer = require('nodemailer');
+const config = require('./config');
 
 const url = 'https://www.amazon.in/JBL-Portable-Wireless-Powerful-Black/dp/B01MSYQWNY/';
 
@@ -16,8 +17,6 @@ async function checkPrice(page){
 
   await page.reload();
   let html = await page.evaluate(() => document.body.innerHTML);
-  // console.log(html);
-  //#priceblock_ourprice
 
   $('#priceblock_ourprice',html).each(function(){
     let price = $(this).text();
@@ -26,6 +25,7 @@ async function checkPrice(page){
 
     if(currentPrice < 7000){
       console.log("Buyy :" + currentPrice);
+      sendNotfication(currentPrice);
     }
   })
 }
@@ -38,6 +38,29 @@ async function startTracking(){
   },null, true, null, null, true);
 
   job.start();
+}
+
+async function sendNotfication(price){
+  var transporter = nodemailer.createTransport({
+    service:'gmail',
+    auth:{
+      user:config.email,
+      pass:config.pass
+    }
+  });
+
+  let textToSend = "Price Dropped to" + price;
+  let htmlText = `<a href=\"${url}\">Link</a>`;
+
+  let info = await transporter.sendMail({
+    from: '"Price Tracker" <photos.rssut@gmail.com>',
+    to:'umang.usict.042164@ipu.ac.in',
+    subject:"Price Dropped to" + price,
+    text:textToSend,
+    html:htmlText
+  });
+
+  console.log("Message Sent: %s",info.messageId);
 }
 
 startTracking();

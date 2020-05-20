121a30cd953475212c1404f7e93c08f93069c416
umang-jain <umang.usict.042164@ipu.ac.in>
2020-05-20 14:17:23 +0530

    add puppeteer

---
+++ app.js
@@ -0,0 +1,38 @@
+const puppeteer = require('puppeteer');
+const $ = require('cheerio');
+const CronJob = require('cron').CronJob;
+const nodemailer = require('nodemailer');
+
+const url = 'https://www.amazon.in/JBL-Portable-Wireless-Powerful-Black/dp/B01MSYQWNY/';
+
+async function configureBrowser(){
+  const browser = await puppeteer.launch();
+  const page = await browser.newPage();
+  await page.goto(url);
+  return page;
+}
+
+async function checkPrice(page){
+
+  await page.reload();
+  let html = await page.evaluate(() => document.body.innerHTML);
+  // console.log(html);
+  //#priceblock_ourprice
+
+  $('#priceblock_ourprice',html).each(function(){
+    let price = $(this).text();
+    let currentPrice = Number(price.replace(/[^0-9.-]+/g,""));
+    // console.log(currentPrice);
+
+    if(currentPrice < 7000){
+      console.log("Buyy :" + currentPrice);
+    }
+  })
+}
+
+async function monitor(){
+  let page = await configureBrowser();
+  await checkPrice(page);
+}
+
+monitor();
